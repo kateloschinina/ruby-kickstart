ec9d9a8ae05e776b110f4f06fe5faa91a16c1824
kateloschinina <ekaterina.loshchinina@gmail.com>
2016-12-01 17:06:50 +0000

    5th challenge, 3rd session

--- session3/3-challenge/5_blocks.rb
+++ session3/3-challenge/5_blocks.rb
@@ -20,6 +20,44 @@
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 
+two_d = [
+   [ 1,  2,  3,  4, 5],
+   [16, 17, 18, 19, 6],
+   [15, 24, 25, 20, 7],
+   [14, 23, 22, 21, 8],
+   [13, 12, 11, 10, 9],
+ ]
 
+=begin
+two_d = [
+   [ 1,  2,  3],
+   [8, 9, 4],
+   [7, 6, 5],
+ ]
+=end
 
+order = []
 
+def spiral(two_d,two_d_res = Array.new)
+  if two_d == []
+    return two_d_res
+  end
+  two_d_res << two_d[0]
+  two_d.shift
+  two_d = two_d.transpose.reverse
+  #puts "two_d: #{two_d}"
+  spiral(two_d,two_d_res)
+end
+
+#puts "spiral: #{spiral(two_d)}"
+
+def spiral_access (two_d, &block)
+  two_d = spiral(two_d)
+  two_d.each {|x| x.each { |y| block.call y}}
+end
+
+spiral_access two_d do |i|
+   order << i**2
+ end
+
+ #puts order
