8476338f4ee44722d75a8660e2f1a3d6b24dc205
kateloschinina <ekaterina.loshchinina@gmail.com>
2016-12-02 01:09:00 +0000

    15th challenge, 3rd session

--- session3/3-challenge/15_hash.rb
+++ session3/3-challenge/15_hash.rb
@@ -46,5 +46,27 @@
 # head = {:data => 6, :next => head}
 # middle head                        # => 3
 
+def list_size(list)
+  return 0 unless list
+  1 + list_size(list[:next])
+end
 
+def middle(list, distance=list_size(list)/2)
+  return list[:data] if distance == 0
+  middle list[:next], (distance-1)
+end
 
+=begin
+head = {:data => 1, :next => nil}
+puts middle head                        # => 1
+head = {:data => 2, :next => head}
+puts middle head                        # => 1
+head = {:data => 3, :next => head}
+puts middle head                        # => 2
+head = {:data => 4, :next => head}
+puts middle head                        # => 2
+head = {:data => 5, :next => head}
+puts middle head                        # => 3
+head = {:data => 6, :next => head}
+puts middle head
+=end
